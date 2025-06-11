c02012d8b87770b9efe6b28ad5bfe6eb25ef3a7c
oskar <oskar.mozdzen@gmail.com>
2025-06-11 18:11:32 +0200

    redirects

--- vox_legatus/app/web/pages.py
+++ vox_legatus/app/web/pages.py
@@ -9,11 +9,27 @@ from app.auth import get_current_user_html, require_role_html
 from app.database import get_db
 from app.enums import Role
 from app.models import Poll, User
-from typing import Annotated
+from typing import Annotated, Union
 from fastapi.responses import RedirectResponse
 
 router = APIRouter()
 templates = Jinja2Templates(directory="./app/templates")
+
+
+@router.get("/", include_in_schema=False)
+async def root(
+    request: Request,
+    current_user: Union[User, RedirectResponse] = Depends(get_current_user_html)
+):
+    # If the user was not authenticated, RedirectResponse will be returned
+    if isinstance(current_user, RedirectResponse):
+        return current_user
+
+    # Redirect based on role
+    if current_user.role == Role.ADMIN:
+        return RedirectResponse("/dashboard")
+    else:
+        return RedirectResponse("/vote")
 
 
 @router.get("/login", response_class=HTMLResponse)
